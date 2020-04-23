90fa071721f72d67944a257800ea63ad42b7122d
evan <aspaasevan@gmail.com>
2020-04-23 16:22:33 -0700

    removed commented axios

--- react-redux-app/src/components/MainMenu.js
+++ react-redux-app/src/components/MainMenu.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 import { connect } from "react-redux";
+import Loader from 'react-loader-spinner'
 import './styles.css';
 
 import { fetchData } from '../store/actions/actions'
 import DrinkCard from './DrinkCard';
 
-const MainMenu = ({fetchData}) => {
+const MainMenu = ({loading, error, fetchData}) => {
 
   useEffect(() => {
     fetchData();
@@ -13,9 +14,27 @@ const MainMenu = ({fetchData}) => {
 
   return (
     <div className="main-menu">
+      {/*  TODO fetching data displays loader if loading state = true */}
+      {
+        loading &&
+          <Loader
+            type="Puff"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={5000} //3 secs
+          />
+      }
       <DrinkCard />
     </div>
   );
 }
 
-export default connect(null, { fetchData })(MainMenu);
+const mapStateToProps = state => {
+  return { 
+    loading: state.loading,
+    error: state.error,
+  }
+};
+
+export default connect(mapStateToProps, { fetchData })(MainMenu);
--- react-redux-app/src/store/actions/actions.js
+++ react-redux-app/src/store/actions/actions.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
 
-export const FETCH_DATA = 'FETCH_DATA';
+export const FETCHING_DATA = 'FETCHING_DATA';
+export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
+export const FETCH_ERROR = 'FETCH_ERROR';
 
 export const fetchData = () => {
   return dispatch => {
+    dispatch({ type: FETCHING_DATA });
     axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
     .then(response => {
-      dispatch({ type: FETCH_DATA, payload: response.data.drinks[0]});
+      dispatch({ type: FETCH_DATA_SUCCESS, payload: response.data.drinks[0]});
     })
     .catch(error => {
-      console.log(error.response)
+      dispatch({ type: FETCH_ERROR, payload: error.response.message })
     })
   }
 };
